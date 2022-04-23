d27b7172c32fe177fe1e1ef26c16947fa60a77b0
Prashant Mohta <mohtaprashantrocks@gmail.com>
2022-04-23 06:16:04 +0530

    add null checks to audio clip extractor

--- Futils/Extractors/AudioClips.cs
+++ Futils/Extractors/AudioClips.cs
@@ -9,41 +9,50 @@ namespace Satchel.Futils.Extractors
                     Type t = action.GetType();
                     if (action is AudioPlay){
                         AudioPlay a = (AudioPlay) action;
+                        if(a.oneShotClip == null){ continue; }
                         var clip = a.oneShotClip.Value as AudioClip;
                         ac[clip.name]=clip;
                     } else if(action is AudioPlayerOneShot){
                         AudioPlayerOneShot a = (AudioPlayerOneShot) action;
                         foreach( var clip in a.audioClips){
+                            if(clip == null){ continue; }
                             ac[clip.name]=clip;
                         }
                     }  else if(action is AudioPlayerOneShotSingle){
                         AudioPlayerOneShotSingle a = (AudioPlayerOneShotSingle) action;
+                        if(a.audioClip == null){ continue; }
                         var clip = a.audioClip.Value as AudioClip;
                         ac[clip.name]=clip;
                     } else if (action is AudioPlayRandom){
                         AudioPlayRandom a = (AudioPlayRandom) action;
                         foreach( var clip in a.audioClips){
+                            if(clip == null){ continue; }
                             ac[clip.name]=clip;
                         }
                     } else if (action is AudioPlayRandomSingle){
                         AudioPlayRandomSingle a = (AudioPlayRandomSingle) action;
+                        if(a.audioClip == null){ continue; }
                         var clip = a.audioClip.Value as AudioClip;
                         ac[clip.name]=clip;
                     } else if (action is AudioPlaySimple){
                         AudioPlaySimple a = (AudioPlaySimple) action;
+                        if(a.oneShotClip == null){ continue; }
                         var clip = a.oneShotClip.Value as AudioClip;
                         ac[clip.name]=clip;
                     } else if (action is AudioPlayV2){
                         AudioPlayV2 a = (AudioPlayV2) action;
+                        if(a.oneShotClip == null){ continue; }
                         var clip = a.oneShotClip.Value as AudioClip;
                         ac[clip.name]=clip;
                     } else if (action is PlayRandomSound){
                         PlayRandomSound a = (PlayRandomSound) action;
                         foreach( var clip in a.audioClips){
+                            if(clip == null){ continue; }
                             ac[clip.name]=clip;
                         }
                     } else if (action is PlaySound){
                         PlaySound a = (PlaySound) action;
+                        if(a.clip == null){ continue; }
                         var clip = a.clip.Value as AudioClip;
                         ac[clip.name]=clip;
                     } 
