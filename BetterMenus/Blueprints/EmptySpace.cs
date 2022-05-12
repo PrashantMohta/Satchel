631628f914cc19f7fa669e6bce7faab09c192a72
Mulhima <81007262+TheMulhima@users.noreply.github.com>
2022-05-12 22:39:29 +0400

    add new blueprint empty space

---
+++ BetterMenus/Blueprints/EmptySpace.cs
@@ -0,0 +1,7 @@
+namespace Satchel.BetterMenus{
+    public static partial class Blueprints{
+        public static Element EmptySpace(){
+          return new StaticPanel("Empty Space", _ => { });
+        }
+    }
+}
