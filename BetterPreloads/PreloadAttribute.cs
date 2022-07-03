abb1b63a375c59e5936330f17831df714ce6bfb9
Prashant Mohta <mohtaprashantrocks@gmail.com>
2022-07-03 20:22:42 +0530

    make attribute members publicly gettable

--- BetterPreloads/PreloadAttribute.cs
+++ BetterPreloads/PreloadAttribute.cs
@@ -2,8 +2,8 @@ namespace Satchel.BetterPreloads{
     
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property,AllowMultiple=false)]   
     public class PreloadAttribute : Attribute{
-        internal string scene;
-        internal string objPath;
+        public string scene {get; protected set;}
+        public string objPath {get; protected set;}
         public PreloadAttribute(string scene, string objPath){
             this.scene = scene;
             this.objPath = objPath;
