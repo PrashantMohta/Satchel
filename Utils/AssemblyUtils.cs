fd9274610f41bd342daedb2771f650af44ecf24c
TheMulhima <mulhima003@gmail.com>
2022-10-27 22:08:11 -0400

    bump version

--- Utils/AssemblyUtils.cs
+++ Utils/AssemblyUtils.cs
@@ -15,7 +15,7 @@ namespace Satchel
         /// <summary>
         /// Version of this Assembly
         /// </summary>
-        public static string ver = "0.8.2";
+        public static string ver = "0.8.4";
         /// <summary>
         /// Gets Version string
         /// </summary>
