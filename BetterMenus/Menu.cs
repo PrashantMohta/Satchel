8cff82354cab9b92f6ec1fa614caa2fe12450504
TheMulhima <mulhima003@gmail.com>
2022-11-10 16:18:53 -0500

    add xml doc for missing param and bump version

--- BetterMenus/Menu.cs
+++ BetterMenus/Menu.cs
@@ -51,6 +51,7 @@ namespace Satchel.BetterMenus
         /// <summary>
         /// Creates a new instance of Menu. This is used to create a better menu
         /// <param name="name">The title of the ModMenu"></param>
+        /// <param name="elements">An array of elements you want to add to the menu. You can add more elements later via MenuRef.AddElement</param>
         /// </summary>
         public Menu(string name, Element[] elements)
         {
