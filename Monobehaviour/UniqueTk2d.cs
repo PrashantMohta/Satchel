946acfda3eb2ebba401bfa07de731fe6179a7a9d
PrashantMohta <mohtaprashantrocks@gmail.com>
2025-06-01 15:53:08 +0700

    Add UniqueTk2d system

---
+++ Monobehaviour/UniqueTk2d.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Satchel
+{
+    /// <summary>
+    /// Monobehavior to manage the uniqueness of a Tk2d animation system
+    /// </summary>
+    public class UniqueTk2d : MonoBehaviour
+    {
+        private tk2dSpriteAnimator animator;
+        private tk2dSpriteAnimation animation, animationClone;
+        private tk2dSpriteCollectionData collection, collectionClone;
+
+        /// <summary>
+        /// Current state of the tk2d system
+        /// </summary>
+        public bool IsUnique { get; private set; } = false;
+
+
+        private void EnsureDefaults()
+        {
+            animator = GetComponent<tk2dSpriteAnimator>();
+            if (animator != null)
+            {
+                if (animation == null)
+                {
+                    animation = animator.Library;
+                }
+                if (collection == null)
+                {
+                    collection = animator.Library.clips[0].frames[0].spriteCollection;
+                }
+            }
+        }
+        private void EnsureClones()
+        {
+            EnsureDefaults();
+            if (animationClone == null)
+            {
+                animationClone = animation.Clone();
+                animationClone.gameObject.transform.SetParent(transform);
+            }
+            if (collectionClone == null)
+            {
+                collectionClone = collection.Clone();
+                collectionClone.gameObject.transform.SetParent(transform);
+                foreach (tk2dSpriteAnimationClip clip in animationClone.clips)
+                {
+                    foreach (tk2dSpriteAnimationFrame frame in clip.frames)
+                    {
+
+                        frame.spriteCollection = collectionClone;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Make the current Tk2d system unique
+        /// </summary>
+        /// <returns>self</returns>
+        public UniqueTk2d MakeUnique()
+        {
+            EnsureClones();
+            animator.Library = animationClone;
+            IsUnique = true;
+            return this;
+        }
+
+        /// <summary>
+        /// Make the current Tk2d system generic
+        /// </summary>
+        /// <returns>self</returns>
+        public UniqueTk2d MakeGeneric()
+        {
+            EnsureDefaults();
+            animator.Library = animation;
+            IsUnique = false;
+            return this;
+        }
+
+        /// <summary>
+        /// Set the texture to the current tk2d animation system
+        /// </summary>
+        /// <param name="tex"></param>
+        public void SetTexture(Texture2D tex)
+        {
+            if (IsUnique)
+            {
+                collectionClone.SetTexture(tex);
+            }
+            else
+            {
+                collection.SetTexture(tex);
+            }
+
+        }
+    }
+
+}
---
+++ Utils/Tk2dUtils.cs
@@ -0,0 +1,82 @@
+﻿using System.Reflection;
+using UObject = UnityEngine.Object;
+
+namespace Satchel
+{
+    /// <summary>
+    /// Utilities to Work with a tk2d system
+    /// </summary>
+    public static class Tk2dUtils
+    {
+        private static FieldInfo[] spriteDefinitionFields;
+        private static Type spriteDefinitionType;
+
+        /// <summary>
+        /// Sets the texture of a tk2dSpriteCollectionData
+        /// </summary>
+        /// <param name="collection">the tk2dSpriteCollectionData to set the texture</param>
+        /// <param name="tex">the texture to set</param>
+        public static void SetTexture(this tk2dSpriteCollectionData collection, Texture tex)
+        {
+
+            if (collection.textures.Length > 0)
+            {
+                collection.textures[0] = tex;
+            }
+            if (collection.materials.Length > 0)
+            {
+                collection.materials[0].mainTexture = tex;
+            }
+            else
+            {
+                collection.material.mainTexture = tex;
+            }
+        }
+
+        /// <summary>
+        /// Clones the tk2dSpriteAnimation passed in so that it can be modified without modifying all instances
+        /// </summary>
+        /// <param name="animation">tk2dSpriteAnimation to clone</param>
+        /// <returns>cloned tk2dSpriteAnimation</returns>
+        public static tk2dSpriteAnimation Clone(this tk2dSpriteAnimation animation)
+        {
+            tk2dSpriteAnimation newAnimation = UObject.Instantiate(animation);
+            for (int i = 0; i < newAnimation.clips.Length; i++)
+            {
+                newAnimation.clips[i] = new tk2dSpriteAnimationClip(newAnimation.clips[i]);
+            }
+
+            newAnimation.name = animation.name;
+            return newAnimation;
+        }
+
+        /// <summary>
+        /// Clones the tk2dSpriteCollectionData passed in so that it can be modified without modifying all instances
+        /// </summary>
+        /// <param name="collection">tk2dSpriteCollectionData to clone</param>
+        /// <returns>cloned tk2dSpriteCollectionData</returns>
+        public static tk2dSpriteCollectionData Clone(this tk2dSpriteCollectionData collection)
+        {
+            tk2dSpriteCollectionData newCollection = UObject.Instantiate(collection);
+            for (int i = 0; i < newCollection.materials.Length; i++)
+            {
+                newCollection.materials[i] = UObject.Instantiate(newCollection.materials[i]);
+            }
+
+            for (int i = 0; i < newCollection.spriteDefinitions.Length; i++)
+            {
+                tk2dSpriteDefinition definition = new tk2dSpriteDefinition();
+                if (spriteDefinitionType == null) spriteDefinitionType = typeof(tk2dSpriteDefinition);
+                if (spriteDefinitionFields == null) spriteDefinitionFields = spriteDefinitionType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                foreach (FieldInfo fi in spriteDefinitionFields)
+                    fi.SetValue(definition, fi.GetValue(newCollection.spriteDefinitions[i]));
+                definition.material = newCollection.materials[definition.materialId];
+                newCollection.spriteDefinitions[i] = definition;
+            }
+
+            newCollection.spriteCollectionName = collection.spriteCollectionName;
+            newCollection.name = collection.name;
+            return newCollection;
+        }
+    }
+}
